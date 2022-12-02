8bdeff87cd12f33bbb0de3dcbf8011d3ffd7c562
DoWhatYouL0ve <william_ross@mailfence.com>
2022-12-02 14:20:49 +0100

    feat: get blogs and display them.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
 import {NextPage} from "next";
+import { use } from "react";
+import {getBlogs} from "../lib/blogs";
+import Image from "next/image";
+
+async function getInitialBlogs() {
+    const blogs = getBlogs()
+    return blogs
+}
 
 const Page: NextPage = () => {
+
+    const blogs = use(getInitialBlogs())
+
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h2 className="sr-only">Blogs</h2>
+
+                <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                    {blogs.map((blogs, index) => (
+                        <a key={index} href={''} className="group">
+                            <div className="relative aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                                <Image
+                                    fill
+                                    src={blogs.coverImage}
+                                    alt={''}
+                                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                                />
+                            </div>
+                            <h3 className="mt-4 text-sm text-gray-700">{blogs.title}</h3>
+                            <p className="mt-1 text-lg font-medium text-gray-900">{blogs.description}</p>
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
